2843f3415dfb6596519e8f1cb53e5c9404b05adc
Craig Puxty <craig_puxty@talktalk.net>
2022-04-03 19:51:03 +0100

    Added background to Header on scrollY.

--- layout/Header.js
+++ layout/Header.js
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import MobileIcon from "./MobileIcon";
 import Navigation from "./Navigation";
 
 export default function Header() {
+  const [background, setBackground] = useState(false);
+
+  const handleScroll = () => {
+    window.scrollY > 200 ? setBackground(true) : setBackground(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
     <>
-      <header className="header">
+      <header className={background ? "header background" : "header"}>
         <div className="container">
           <div className="logo">Craig Puxty</div>
           <Navigation />
