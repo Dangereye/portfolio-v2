371b2a33710fed3297461d2d9e226be547ff7fd4
Craig Puxty <craig_puxty@talktalk.net>
2022-04-03 14:47:38 +0100

    Updated various focus styles

--- layout/MobileIcon.js
+++ layout/MobileIcon.js
@@ -4,10 +4,18 @@ import { AppContext } from "../context/AppContext";
 export default function MobileIcon() {
   const { menuIsOpen, setMenuIsOpen } = useContext(AppContext);
 
+  const handleKeyDown = (e) => {
+    if (e.code === "Enter") {
+      setMenuIsOpen(!menuIsOpen);
+    }
+  };
+
   return (
     <div
+      tabIndex="0"
       className={menuIsOpen ? "mobile-icon open" : "mobile-icon"}
       onClick={() => setMenuIsOpen(!menuIsOpen)}
+      onKeyDown={handleKeyDown}
     >
       <div></div>
       <div></div>
