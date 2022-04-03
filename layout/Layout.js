ba98f948ad1a71dd70a208e4795114e5574a7394
Craig Puxty <craig_puxty@talktalk.net>
2022-04-03 14:58:30 +0100

    Updated Layout to close mobile menu (if open) on route change.

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,3 +1,15 @@
+import { AppContext } from "../context/AppContext";
+import { useContext, useEffect } from "react";
+import { useRouter } from "next/router";
 export default function Layout({ children }) {
+  const { menuIsOpen, setMenuIsOpen } = useContext(AppContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (menuIsOpen) {
+      setMenuIsOpen(false);
+    }
+  }, [router.asPath]);
+
   return <div className="layout">{children}</div>;
 }
