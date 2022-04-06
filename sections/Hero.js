f78ae7536b7e6ddf024aaca6e063d61e6996782f
Craig Puxty <craig_puxty@talktalk.net>
2022-04-06 19:18:20 +0100

    Added Next & React SVGs.

--- sections/Hero.js
+++ sections/Hero.js
@@ -4,18 +4,28 @@ export default function Hero() {
   return (
     <section className="section section__hero">
       <div className="container">
-        <h1 className="display-heading">
-          <span>Front-End</span>
-          <span>Developer</span>
-        </h1>
-        <p className="body-text">
-          Not your average developer - get ready to transform your ideas into
-          reality.
-        </p>
-        <div className="btns">
-          <Link href="/#portfolio">
-            <a className="btn btn--primary">Recent Projects</a>
-          </Link>
+        <div className="typography-and-cta">
+          <h1 className="display-heading">
+            <span>Front-End</span>
+            <span>Developer</span>
+          </h1>
+          <p className="body-text">
+            Not your average developer - get ready to transform your ideas into
+            reality.
+          </p>
+          <div className="btns">
+            <Link href="/#portfolio">
+              <a className="btn btn--primary">Recent Projects</a>
+            </Link>
+          </div>
+        </div>
+        <div className="highlights">
+          <div className="highlight">
+            <div className="highlight__icon">
+              10<span>+</span>
+            </div>
+            <div className="highlight__text">Years Experience</div>
+          </div>
         </div>
       </div>
     </section>
