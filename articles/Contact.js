1d8b4e7a12873a51c93042d0d1c6346b9a36a4ba
Craig Puxty <craig_puxty@talktalk.net>
2022-07-10 22:40:35 +0100

    Reduced btn--large. Added Contact article barebones & styles.

--- articles/Contact.js
+++ articles/Contact.js
@@ -1,7 +1,39 @@
+import ArticleHeading from "../components/ArticleHeading";
+import BgHighlight from "../components/BgHighlight";
+import Button from "../components/Button";
+
 export default function Contact() {
   return (
-    <section className="section" id="contact">
-      <h2>Contact</h2>
-    </section>
+    <article className="article article__contact" id="contact">
+      <BgHighlight pos="top-left" />
+      <BgHighlight pos="bottom-right" />
+      <div className="container">
+        <header className="article__header">
+          <ArticleHeading lead="Contact" h2="Let's talk!" />
+          <p className="body-text">
+            To discuss career & freelance opportunities, contact me directly or
+            fill out the form and I'll get back to you promptly.
+          </p>
+        </header>
+
+        <form className="form">
+          <div className="form__input-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+          <div className="form__input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className="form__textarea-group">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" />
+          </div>
+          <div className="btns">
+            <Button classes="btn--primary btn--large" name="Send message" />
+          </div>
+        </form>
+      </div>
+    </article>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
           {/* <Photoshop /> */}
           {/* <Skills /> */}
           {/* <About /> */}
-          {/* <Contact /> */}
+          <Contact />
         </>
       ) : (
         <Slideshow />
