c4e50b5aeeefb2d422e9fe08e8b67f64b3a24b91
Craig Puxty <craig_puxty@talktalk.net>
2022-07-20 21:18:32 +0100

    Updated BgHighlight component styles. Updated Layout of Hero article.

--- articles/hero/Hero.js
+++ articles/hero/Hero.js
@@ -10,41 +10,37 @@ import Feature from "./Feature";
 export default function Hero() {
   return (
     <article className="article article__hero">
-      <BgHighlight pos="top-left" />
-      <BgHighlight pos="bottom-right" />
+      <BgHighlight pos="top-center" />
       <div className="container">
-        <div className="content">
-          <div className="typography">
-            <div className="heading heading--lead">Front-end developer</div>
-            <h1 className="heading heading--h1">
-              <div>I build things</div>
-              <div>
-                for the <span>Web</span>
-              </div>
-            </h1>
-            <p className="body-text">
-              I love building (and occasionally designing) exceptional user
-              experiences. My niche concerns <span>React JS</span> &amp;{" "}
-              <span>Next JS</span> orientated development.
-            </p>
-            <div className="btns">
-              <Link href="/#portfolio">
-                <a className="btn btn--primary btn--large">Latest projects</a>
-              </Link>
+        <div className="typography">
+          <div className="heading heading--lead">Front-end developer</div>
+          <h1 className="heading heading--h1">
+            <div>I build things</div>
+            <div>
+              for the <span>Web</span>
             </div>
-          </div>{" "}
-          <div className="image">
-            <div className="image__wrapper">
-              <Image src="/img/hero/profile.png" width="800" height="800" />
-            </div>
+          </h1>
+          <p className="body-text">
+            I love building (and occasionally designing) exceptional user
+            experiences. My niche concerns <span>React JS</span> &amp;{" "}
+            <span>Next JS</span> orientated development.
+          </p>
+          <div className="btns">
+            <Link href="/#portfolio">
+              <a className="btn btn--primary btn--large">Latest projects</a>
+            </Link>
           </div>
         </div>
-
         <div className="features">
           <Feature icon={<ReactSvg />} text="React JS focused" />
           <Feature icon={<NextSVG />} text="Enhanced with Next JS" />
           <Feature icon={<CompassSvg />} text="Modern designs" />
           <Feature icon={<ScreenSvg />} text="Device responsive" />
+        </div>
+        <div className="image">
+          <div className="image__wrapper">
+            <Image src="/img/hero/profile.png" width="800" height="800" />
+          </div>
         </div>
       </div>
     </article>
