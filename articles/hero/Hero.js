8e507b7bd6a3440709605e84b1ac35d48d3d3b87
Craig Puxty <craig_puxty@talktalk.net>
2022-08-05 18:51:59 +0100

    Added basic setup for Hero article animation.

--- articles/hero/Hero.js
+++ articles/hero/Hero.js
@@ -5,25 +5,47 @@ import ReactSvg from "../../svgs/React";
 import ScreenSvg from "../../svgs/Screens";
 import Feature from "./Feature";
 
+import { useEffect } from "react";
+
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Hero() {
+  useEffect(() => {
+    const animate = gsap.from(".hero-animate", {
+      opacity: 0,
+      y: -100,
+      stagger: 0.1,
+      duration: 0.3,
+      delay: 1,
+      scrollTrigger: "#hero",
+    });
+    return () => {
+      animate.kill();
+    };
+  }, []);
   return (
-    <article className="article article__hero">
+    <article className="article article__hero" id="hero">
       <div className="background"></div>
       <div className="container">
         <div className="typography">
-          <div className="heading heading--lead">Front-end developer</div>
+          <div className="heading heading--lead hero-animate">
+            Front-end developer
+          </div>
           <h1 className="heading heading--h1">
-            <div>I build things</div>
-            <div>
+            <div className="hero-animate">I build things</div>
+            <div className="hero-animate">
               for the <span>Web</span>
             </div>
           </h1>
-          <p className="body-text">
+          <p className="body-text hero-animate">
             I love building (and occasionally designing) exceptional user
             experiences. My niche concerns <span>React JS</span> &amp;{" "}
             <span>Next JS</span> orientated development.
           </p>
-          <div className="btns">
+          <div className="btns hero-animate">
             <Link href="/#projects">
               <a className="btn btn--primary btn--large">Latest projects</a>
             </Link>
