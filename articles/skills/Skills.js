d2402840233768523259bdd081e4c3b81617f4a6
Craig Puxty <craig_puxty@talktalk.net>
2022-08-07 19:24:56 +0100

    Added ListGroup anim prop. Added Skills article animation.

--- articles/skills/Skills.js
+++ articles/skills/Skills.js
@@ -4,15 +4,23 @@ import { environment } from "../../data/skills/environment";
 import { designTools } from "../../data/skills/designTools";
 import { frontEnd } from "../../data/skills/frontEnd";
 import { backEnd } from "../../data/skills/backEnd";
+import useScaleDown from "../../hooks/animation/useScaleDown";
+import useFadeDown from "../../hooks/animation/useFadeDown";
 
 export default function Skills() {
+  const {} = useScaleDown(".skills-animate-bg", "#skills");
+  const {} = useFadeDown(".skills-animate", "#skills");
   return (
     <article className="article article__skills" id="skills">
-      <div className="container">
-        <div className="background"></div>
+      <div className="container skills-animate-bg">
+        <div className="background "></div>
         <header className="article__header">
-          <ArticleHeading lead="Skills" h2="Knowledge & Experience" />
-          <p className="body-text">
+          <ArticleHeading
+            lead="Skills"
+            h2="Knowledge & Experience"
+            anim="skills-animate"
+          />
+          <p className="body-text skills-animate">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
             tenetur maxime, tempore aspernatur aperiam ut fugit recusandae odit
             corrupti at excepturi doloribus, tempora non? Autem assumenda neque
@@ -23,15 +31,27 @@ export default function Skills() {
           <ListGroup
             heading={environment.heading}
             list={environment.list}
+            anim="skills-animate"
             image
           />
           <ListGroup
             heading={designTools.heading}
             list={designTools.list}
+            anim="skills-animate"
             image
           />
-          <ListGroup heading={frontEnd.heading} list={frontEnd.list} image />
-          <ListGroup heading={backEnd.heading} list={backEnd.list} image />
+          <ListGroup
+            heading={frontEnd.heading}
+            list={frontEnd.list}
+            anim="skills-animate"
+            image
+          />
+          <ListGroup
+            heading={backEnd.heading}
+            list={backEnd.list}
+            anim="skills-animate"
+            image
+          />
         </div>
       </div>
     </article>
--- components/ListGroup.js
+++ components/ListGroup.js
@@ -1,11 +1,16 @@
 import PropTypes from "prop-types";
-export default function ListGroup({ heading, list, image }) {
+export default function ListGroup({ heading, list, anim, image }) {
   return (
     <div className="list-group">
-      <h3 className="heading heading--h3">{heading}</h3>
+      <h3 className={`heading heading--h3 ${anim}`}>{heading}</h3>
       <ul className="list">
         {list.map((item) => (
-          <li key={item} className={image ? "list__item image" : "list__item"}>
+          <li
+            key={item}
+            className={
+              image ? `list__item image ${anim}` : `list__item ${anim}`
+            }
+          >
             {item}
           </li>
         ))}
@@ -17,6 +22,7 @@ export default function ListGroup({ heading, list, image }) {
 ListGroup.defaultProps = {
   heading: "Lists heading",
   list: ["List item 01", "List item 02", "List item 03", "List item 04"],
+  anim: "",
   image: false,
 };
 
@@ -25,5 +31,6 @@ ListGroup.propTypes = {
   list: PropTypes.arrayOf(
     PropTypes.oneOfType([PropTypes.string, PropTypes.element])
   ),
+  anim: PropTypes.string,
   image: PropTypes.bool,
 };
