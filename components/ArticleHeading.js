d075132be8155fe1bd608caf2815e8afdd3316b7
Craig Puxty <craig_puxty@talktalk.net>
2022-08-07 19:13:27 +0100

    Added ArticleHeading PropTypes and anim prop.

--- components/ArticleHeading.js
+++ components/ArticleHeading.js
@@ -1,8 +1,9 @@
-export default function ArticleHeading({ lead, h2 }) {
+import PropTypes from "prop-types";
+export default function ArticleHeading({ lead, h2, anim }) {
   return (
     <>
-      <div className="heading heading--lead">{lead}</div>
-      <h2 className="heading heading--h2">{h2}</h2>
+      <div className={`heading heading--lead ${anim}`}>{lead}</div>
+      <h2 className={`heading heading--h2 ${anim}`}>{h2}</h2>
     </>
   );
 }
@@ -10,4 +11,11 @@ export default function ArticleHeading({ lead, h2 }) {
 ArticleHeading.defaultProps = {
   lead: "first heading ",
   h2: "Second heading",
+  anim: "",
+};
+
+ArticleHeading.propTypes = {
+  lead: PropTypes.string,
+  h2: PropTypes.string,
+  anim: PropTypes.string,
 };
