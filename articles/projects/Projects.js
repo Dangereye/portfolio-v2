a230d23bc4001d2bd48b6ef22361362d3f59b62a
Craig Puxty <craig_puxty@talktalk.net>
2022-08-07 19:45:13 +0100

    Added Projects article animation.

--- articles/projects/Projects.js
+++ articles/projects/Projects.js
@@ -3,14 +3,20 @@ import { games } from "../../data/projects/games_data";
 import { movies } from "../../data/projects/movies_data";
 import { the_weather } from "../../data/projects/the_weather_data";
 import ArticleHeading from "../../components/ArticleHeading";
+import useFadeDown from "../../hooks/animation/useFadeDown";
 
 export default function Projects() {
+  const {} = useFadeDown(".projects-animate", "#projects");
   return (
     <article className="article article__projects" id="projects">
       <div className="container">
         <header className="article__header">
-          <ArticleHeading lead="projects" h2="Latest projects" />
-          <p className="body-text">
+          <ArticleHeading
+            lead="projects"
+            h2="Latest projects"
+            anim="projects-animate"
+          />
+          <p className="body-text projects-animate">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
             tenetur maxime, tempore aspernatur aperiam ut fugit recusandae odit
             corrupti at excepturi doloribus, tempora non? Autem assumenda neque
