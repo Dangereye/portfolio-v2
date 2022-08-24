c96e4645830a9b3ce3abcdf8d47a60ec9891605d
Craig Puxty <craig_puxty@talktalk.net>
2022-08-24 17:52:04 +0100

    Removed unused animation hooks and added comments to Footer.

--- layout/Footer.js
+++ layout/Footer.js
@@ -1,44 +1,47 @@
+// Components
+import Link from "next/link";
 import ListGroup from "../components/ListGroup";
 import LinkGroup from "../components/LinkGroup";
+import IconText from "../components/IconText";
+import AnchorGroup from "../components/AnchorGroup";
+import DownloadGroup from "../components/DownloadGroup";
+
+// Data
 import { Pages } from "../data/Pages";
 import { Connect } from "../data/Connect";
 import { SourceCode } from "../data/SourceCode";
 import { Downloads } from "../data/Downloads";
+
+// Icons
 import { MdEmail } from "react-icons/md";
 import { BiArrowToTop } from "react-icons/bi";
-import IconText from "../components/IconText";
-import AnchorGroup from "../components/AnchorGroup";
-import Link from "next/link";
-import DownloadGroup from "../components/DownloadGroup";
 
 export default function () {
   return (
-    <>
-      <footer className="footer">
-        <Link href="/">
-          <a className="footer__back-to-top">
-            <BiArrowToTop />
-          </a>
-        </Link>
+    <footer className="footer">
+      <Link href="/">
+        <a className="footer__back-to-top">
+          <BiArrowToTop />
+        </a>
+      </Link>
 
-        <div className="container">
-          <div className="footer__links">
-            <LinkGroup heading="Pages" list={Pages} />
-            <AnchorGroup heading="Connect" list={Connect} />
-            <AnchorGroup heading="Source Code" list={SourceCode} />
-            <DownloadGroup heading="Downloads" list={Downloads} />
-            <ListGroup
-              heading="Contact"
-              list={[
-                <IconText icon={<MdEmail />} text="craig_puxty@talktalk.net" />,
-              ]}
-            />
-          </div>
-          <div className="copyright">
-            &copy; Craig Puxty - 2022. All Rights Reserved.
-          </div>
+      <div className="container">
+        <div className="footer__links">
+          <LinkGroup heading="Pages" list={Pages} />
+          <AnchorGroup heading="Connect" list={Connect} />
+          <AnchorGroup heading="Source Code" list={SourceCode} />
+          <DownloadGroup heading="Downloads" list={Downloads} />
+          <ListGroup
+            heading="Contact"
+            list={[
+              <IconText icon={<MdEmail />} text="craig_puxty@talktalk.net" />,
+            ]}
+          />
         </div>
-      </footer>
-    </>
+        <div className="copyright">
+          &copy; Craig Puxty - 2022. All Rights Reserved.
+        </div>
+      </div>
+    </footer>
   );
 }
